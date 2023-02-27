f32791ef8ff9ac9de983b2c3adc1fe697241ad0e
Darth Vader <danieltarancon@gmail.com>
2023-02-27 18:39:46 +0100

    Update prk.py

--- prk.py
+++ prk.py
@@ -1,6 +1,51 @@
-print(set(range(1,11)))
-s = set(range(1,11))
-for n in s:
-    print(n)
+import math
+import numpy
+import pdb
+
+a = 1
+b = 2
+c = 1000 - a - b
+final = []
+
+#while (a**2 + 2**b) != (1000-a-b)**2:
+    #a +=1
+    #while b <= a:
+        #b += 1
+
+
+
+while (a**2 + 2**b) != (1000-a-b)**2:
+    for n in list(range(1,1001)):
+        a = n
+        b = 2
+        while b <= a:
+            b +=1
+        for m in list(range(b,1001)):
+            b = m
+            if (a**2 + 2**b) != (1000-a-b)**2:
+                final = [a,b,c]
+            else:
+                continue
+
+final[0]*final[1]*final[2]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
